97e43168cee491b69c5aa8e9df1facbab26adbbe
renjinxian <jinxian.ren@hand-china.com>
2024-05-31 13:33:27 +0800

    跨域配置

--- src/components/FileUpload.vue
+++ src/components/FileUpload.vue
@@ -2,7 +2,8 @@
   <el-upload
       class="upload-demo"
       drag
-      action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15"
+      action="http://127.0.0.1:8080/image/upload"
+      name="image"
       multiple
   >
     <el-icon class="el-icon--upload"><upload-filled /></el-icon>
