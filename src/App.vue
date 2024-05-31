8b1a0e57bebeaccaad847d95f834640a470d5fcb
renjinxian <jinxian.ren@hand-china.com>
2024-05-31 11:33:20 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import FileUpload from './components/FileUpload.vue'
+</script>
+
+<template>
+
+  <div style="width: 500px;height: 500px">
+    <FileUpload />
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
